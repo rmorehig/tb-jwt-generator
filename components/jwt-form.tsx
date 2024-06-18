1e84b483c72b415f380f9763777d328e84cac3bf
Rafa Moreno <rmoreno@tinybird.co>
2024-06-18 23:54:48 +0200

    generate token

--- components/jwt-form.tsx
+++ components/jwt-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import jwt from 'jsonwebtoken'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -11,7 +12,6 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 
@@ -21,7 +21,7 @@ const formSchema = z.object({
   name: z.string().min(1, 'Field is required'),
 })
 
-export function JwtForm() {
+export function JwtForm({ onSubmit }: { onSubmit: (jwt: string) => void }) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -31,15 +31,36 @@ export function JwtForm() {
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  function generateJwt(values: z.infer<typeof formSchema>) {
+    const { workspace_id, signing_key, name } = values
+
+    const next10minutes = new Date()
+    next10minutes.setTime(next10minutes.getTime() + 1000 * 60 * 10)
+
+    const payload = {
+      workspace_id,
+      name,
+      exp: next10minutes.getTime() / 1000,
+      scopes: [
+        {
+          type: 'DATASOURCES:READ',
+          resource: 'ea47fd9d-bc48-420d-a6f6-95481fbb3f7c',
+          fixed_params: {
+            org_id: 'vercel',
+          },
+        },
+      ],
+    }
+    const result = jwt.sign(payload, signing_key)
+    onSubmit(result)
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full">
+      <form
+        onSubmit={form.handleSubmit(generateJwt)}
+        className="space-y-4 w-full"
+      >
         <FormField
           control={form.control}
           name="workspace_id"
