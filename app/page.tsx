c50ef3b253e7d9c3d8fde48a72726a919fb62b5b
Rafa Moreno <rmoreno@tinybird.co>
2024-06-18 23:55:46 +0200

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
+'use client'
+
 import { JwtForm } from '@/components/jwt-form'
 import { JwtResult } from '@/components/jwt-result'
+import { useState } from 'react'
 
 export default function Home() {
+  const [jwt, setJwt] = useState<string | null>(null)
   return (
     <main className="flex min-h-screen flex-col items-center gap-8 p-24">
       <h1 className="text-3xl font-bold text-left text-secondary-foreground w-[400px]">
         Tinybird JWT Generator
       </h1>
       <div className="flex flex-col items-center space-y-8 w-[400px]">
-        <JwtForm />
-        <JwtResult />
+        <JwtForm onSubmit={setJwt} />
+        <JwtResult jwt={jwt} />
       </div>
     </main>
   )
--- components/jwt-result.tsx
+++ components/jwt-result.tsx
@@ -1,7 +1,7 @@
-export function JwtResult({ data }: { data?: object }) {
+export function JwtResult({ jwt }: { jwt: string | null }) {
   return (
     <div className="relative rounded font-mono text-sm p-4 bg-foreground text-primary-foreground w-full h-[400px]">
-      <pre>{data ? JSON.stringify(data, null, 2) : 'No data'}</pre>
+      <pre>{jwt ? JSON.stringify(jwt, null, 2) : 'No JWT'}</pre>
     </div>
   )
 }
