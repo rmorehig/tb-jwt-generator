d06f495f3df0b787854ce4bb8e383b21589b34a4
Rafa Moreno <rmoreno@tinybird.co>
2024-06-19 00:02:28 +0200

    refactor

--- components/jwt-result.tsx
+++ components/jwt-result.tsx
@@ -1,7 +1,7 @@
 export function JwtResult({ jwt }: { jwt: string | null }) {
   return (
     <div className="relative rounded font-mono text-sm p-4 bg-foreground text-primary-foreground w-full h-[400px]">
-      <pre>{jwt ? JSON.stringify(jwt, null, 2) : 'No JWT'}</pre>
+      <pre>{jwt || 'No JWT'}</pre>
     </div>
   )
 }
